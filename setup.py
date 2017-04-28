b686a62330f6d2744813e91bf4406dad3cb80fed
Shiran <dudy@ohsu.edu>
2017-04-28 12:24:40 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 
-setup(name="BitWeight",
+setup(name="eBitWeight",
       version="0.1",
       description="Underflow-proof floating-poing math for NLP",
-      author="Kyle Gorman and Steven Bedrick",
-      author_email="gormanky@ohsu.edu",
+      author="Kyle Gorman, Steven Bedrick, and Shiran Dudy",
+      author_email="dudy@ohsu.edu",
       install_requires=["Cython >= 0.22"],
-      ext_modules=cythonize([Extension("bitweight", ["bitweight.pyx"],
+      ext_modules=cythonize([Extension("ebitweight", ["ebitweight.pyx"],
                                        language="c++",
                                        extra_compile_args=["-std=c++11"])]),
-      test_suite="bitweight_test",
+      test_suite="ebitweight_test",
 )
